69286b92b21b203eb556cbc893cf1fa9677223b3
Ricardo <ricardocb007@gmail.com>
2022-10-04 17:28:20 -0500

    Agregar y eliminar articulos de la seccion del carrito

--- scriptcarrito.js
+++ scriptcarrito.js
@@ -1,32 +1,44 @@
 var carrito=0;
-var carritospan=document.getElementById("carito");
-var contenedorcarrito=document.getElementById("carrocompras");
-function agregaralCarrito(imagenSrc){
-    carrito++;
-    carritospan.innerHTML=carrito;
+var contart=0;
 
+var carritospan=document.getElementById("carrito");
+
+//aqui se inyectara el contenido
+var contenedorcarrito=document.getElementById("carrocompras");
+function agregaralCarrito(imagenSrc, artnombre){
+    carrito++;
+    carritospan.innerHTML="Articulos: "+carrito;
+//crear elementos para inyectar
     var articulo=document.createElement("div");
+    articulo.classList.add("articuloCarrito");
+    articulo.setAttribute("id","idArt"+contart++)
+
     var imagen=document.createElement("img");
-    imagen.src="RUTADELAIMAGEN";
+    imagen.src= imagenSrc;
+
+    var nombre = document.createElement("p")
+    nombre.innerHTML = artnombre;
 
     var boton=document.createElement("button");
     //boton.onclick="eliminar()";
     boton.addEventListener("click",function(){
-        eliminar();
+        eliminar(this.parentElement.id);
     })
     boton.innerHTML="Eliminar";
 
     articulo.appendChild(imagen);
+    articulo.appendChild(nombre);
     articulo.appendChild(boton);
 
     //inyecta articulo
-    contenedorcarrito.appendChild(articulo);
+    carrocompras.appendChild(articulo);
 
 }
 
-function eliminar(){
+function eliminar(id){
+    if(carrito>0)
     carrito--;
     carritospan.innerHTML=carrito;
-
-    
+    var articuloeliminado = document.getElementById(id)
+    articuloeliminado.remove();
 }
